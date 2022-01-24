a87d9f485fe645ab5dbd73d65ef6fb584d25df4f
Augusto Madeira Splett (Tinho) <augusto.splett@gmail.com>
2022-01-24 13:56:27 -0300

    Adicionar arquivos de projeto.

---
+++ Localiza.Frotas/Controllers/SingletonController.cs
@@ -0,0 +1,24 @@
+﻿using Localiza.Frotas.Infra.Singleton;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Localiza.Frotas.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class SingletonController : Controller
+    {
+        private readonly SingletonContainer singletonContainer;
+        public SingletonController(SingletonContainer singletonContainer)
+        {
+            this.singletonContainer = singletonContainer;
+        }
+
+        [HttpGet()]
+        public IActionResult Get()
+        {
+            //var singleton = singletonContainer;
+
+            return Ok(singletonContainer);
+        }
+    }
+}
