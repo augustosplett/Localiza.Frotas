4b61d289a14bff2d056eb85d1db0967a28a41425
Augusto Madeira Splett (Tinho) <augusto.splett@gmail.com>
2022-01-24 18:07:26 -0300

    implementado repositorio in memory

---
+++ Localiza.Frotas.Infra/Repository/InMemoryRepository.cs
@@ -0,0 +1,34 @@
+﻿using Localiza.Frotas.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Localiza.Frotas.Infra.Repository
+{
+    public class InMemoryRepository : IVeiculoRepository
+    {
+        private readonly IList<Veiculo> veiculoList = new List<Veiculo>();
+        public void Add(Veiculo veiculo) => veiculoList.Add(veiculo);
+
+
+        public void Delete(Veiculo veiculo) => veiculoList.Remove(veiculo);
+
+
+        public IEnumerable<Veiculo> GetAll()
+        {
+            return veiculoList.ToList();
+        }
+
+        public Veiculo GetById(Guid id) => veiculoList.SingleOrDefault(x => x.Id == id);
+
+
+        public void Update(Veiculo veiculo)
+        {
+            veiculoList.Remove(GetById(veiculo.Id));
+            veiculoList.Add(veiculo);
+        }
+
+        } 
+    }
